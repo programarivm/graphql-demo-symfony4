cea0671adc82c759622983fdd50e540608e34edf
programarivm <info@programarivm.com>
2018-07-07 22:16:35 +0100

    /graphql responds with [{"message":"Schema class  does not exist"}]

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Youshido\GraphQLBundle\GraphQLBundle::class => ['all' => true],
 ];
