5fc724bd9515632372c6c7d34849e0fc07c94126
programarivm <info@programarivm.com>
2018-07-08 19:39:11 +0100

    monolog is installed

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Youshido\GraphQLBundle\GraphQLBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
